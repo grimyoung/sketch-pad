b70334276646d8e27ec740202f696d419aa283dd
Samuel Kim <s4muelk1m@gmail.com>
2016-05-30 14:41:50 -0700

    added button to jQuery script

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Using jQuery/Javascript effects to highlight tiles on a webpage
 
 http://www.theodinproject.com/web-development-101/javascript-and-jquery
+
+Note: Intentionally used .css() instead of using a separated css file for jQuery practice.
--- jQuery.js
+++ jQuery.js
@@ -1,15 +1,32 @@
 $(document).ready(function(){
+    var count = 16;
+    var length = 45;
+    var edge = 1;
+    createGrid(count,length,edge);
+    makeButton();
+    $(document).on("mouseenter",".box",function(){
+        //could also use a "highlighted" class in css and use ".addClass()"
+        $(this).css({"background-color":"blue",
+                    "border-color":"blue"});
+    });
+    $("button").on("click",function(){
+        $("#container").remove();
+        //todo add prompt then figure out how to resize boxes smaller while keeping same amount of pixels
+        createGrid(count,length,edge);
+    });
+});
+
+function createGrid(numbox,boxsize,bordersize){
     $("body").append("<div id='container'></div>")
-    var numbox = 16;
-    var boxsize = 45;
-    var bordersize = 1;
     var length = numbox*boxsize +((numbox)*2)*bordersize;
     $("#container").css({"height":length,
                         "width":length,
                     "border-style": "solid",
                     "border-color":"black",
                     "border-width":bordersize,
-                    "margin":"auto"});
+                    "margin":"auto",
+                    "position":"relative",
+                    "top":"30px"});
     for(var i = 0; i<(numbox*numbox);i++){
         $("#container").append("<div class=box></div>")
     }
@@ -19,9 +36,9 @@ $(document).ready(function(){
                         "border-color":"black",
                         "border-width": bordersize,
                         "float":"left"});
-    $(".box").on("mouseenter",function(){
-        //could also use a "highlighted" class in css and use ".addClass()"
-        $(this).css({"background-color":"blue",
-                    "border-color":"blue"});
-    });
-});
+};
+
+function makeButton(){
+    $("body").prepend("<div id='genButton'><button type='button'>Test</button></div>")
+    $("#genButton").css({"text-align":"center"});
+};
